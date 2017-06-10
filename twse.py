7e9b8c86c12ab24a9a4d350a9d6d5840ec04f88f
Lee Shiueh <lee.shiueh@gmail.com>
2017-06-10 15:35:49 +0800

    revise subcommands and arguments design

--- twse.py
+++ twse.py
@@ -8,14 +8,15 @@ logger = logging.getLogger(__name__)
 
 class TWSE(object):
     
-    def __init__(self):
+    def __init__(self, logger=None):
+        self.logger = logger or logging.getLogger(__name__)
         parser = argparse.ArgumentParser(
             description='TWSE Site (http://www.twse.com.tw/) Parser',
             usage='''twse <command> [<args>]
             
 The most commonly use twse commands are:
-    stock_list        List all stock id and name from TWSE
-    stock_update      Download stock trading data from TWSE
+    list                List all stock market companies id and name from TWSE
+    update <id> <3y>    Download last 3 years market trading exchange daily data for company <id> from TWSE
 '''
             )
         parser.add_argument('command',
@@ -24,31 +25,39 @@ The most commonly use twse commands are:
         # exclude the rest of the args too, or validation will fail
         args = parser.parse_args(sys.argv[1:2])
         if not hasattr(self, args.command):
-            logger.error('Unrecognized command %s' % args.command)
+            self.logger.error('Unrecognized command %s' % args.command)
             parser.print_help()
             exit(1)
         # use dispatch pattern to invoke method with same name
         getattr(self, args.command)()
         
-    def stock_list(self):
+    def list(self):
         parser = argparse.ArgumentParser(
             description='List all stock id and name from TWSE'
             )
-        logger.debug('stock_list cmd execution')
-        print('stock_list cmd execution')
+        parser.add_argument('--stock',
+                            action='store_true',
+                            help='Stock market',
+                            default=False)
+        parser.add_argument('--otc',
+                            action='store_true',
+                            help='OTC market',
+                            default=False)
+        args = parser.parse_args(sys.argv[2:])
+        self.logger.info('list market info: stock %s, otc %s' % (args.stock,args.otc))
     
-    def stock_update(self):
+    def update(self):
         parser = argparse.ArgumentParser(
             description='Download stock trading data from TWSE'
             )
-        parser.add_argument('stock_id')
-        parser.add_argument('period')
+        parser.add_argument('id', help='company id')
+        parser.add_argument('period', help='fetch data period: 3m or 3y; m for month, y for year')
         args = parser.parse_args(sys.argv[2:])
-        print('stop_update execution with stock %s and period %s' % (
-            args.stock_id,args.period
+        self.logger.info('update market trading exchange data for id %s and period %s' % (
+            args.id,args.period
             ))
     
 if __name__ == '__main__':
-    
+    logging.basicConfig(level=logging.DEBUG)
     TWSE()
     
